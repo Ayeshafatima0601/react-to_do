a83bc7ec52a1e38f13d843374a208e5e07e53b41
Ayeshafatima0601 <ayeshafatima0601@gmail.com>
2024-05-30 20:06:54 +0530

    to_do

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import CreatePage from './Components/CreatePage';
+import EditPage from './Components/EditPage';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const editTodo = (id, updatedTodo) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, text: updatedTodo } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <Router>
+      <div className="app-container">
+        <h1 className="app-title">Todo List</h1>
+        <Routes>
+          <Route path="/" element={<HomePage todos={todos} editTodo={editTodo} deleteTodo={deleteTodo} />} />
+          <Route path="/create" element={<CreatePage addTodo={addTodo} />} />
+          <Route path="/edit/:id" element={<EditPage todos={todos} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/EditPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+
+function EditPage({ todos }) {
+    const { id } = useParams();
+    const todoToEdit = todos.find(todo => todo.id === parseFloat(id));
+    const [todo, setTodo] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (todoToEdit) {
+            setTodo(todoToEdit.text);
+        }
+    }, [todoToEdit]);
+
+    const handleChange = (event) => {
+        setTodo(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Edited todo:", todo);
+        navigate('/');
+    };
+
+    return (
+        <div className="edit-page-container">
+            <form onSubmit={handleSubmit}>
+                <label>Edit Todo:
+                <input id="todoInput" type="text" value={todo} onChange={handleChange} />
+                </label>
+                <button type="submit" className="save-btn">Save</button>
+            </form>
+            <Link to="/" className="back-link">Back to Home</Link>
+        </div>
+    );
+}
+
+export default EditPage;
